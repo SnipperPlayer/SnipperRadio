81e329772a3cc7348de6d65e9aabf2e489be9d51
SnipperPlayer <galeana2002@hotmail.com>
2024-11-21 17:16:59 -0800

    New Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+//import Image from "next/image";
+import Playboard from './Playboard';
+import './Page.css'
 
 export default function Home() {
   return (
-    <div>The Best Radio</div>
+    <div className='container'>
+      <div className="left">
+        The Best Radio
+      </div>
+      <div className="right">
+        <div className="top">
+          TOP text
+        </div>
+        <div className="bottom">
+          Bottom text
+        </div>
+      </div>
+    </div>
+
   );
 }
