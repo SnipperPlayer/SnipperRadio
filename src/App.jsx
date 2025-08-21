a6515c3132e19af43e4ad3d5716cecceaa72d33c
SnipperPlayer <galeana2002@hotmail.com>
2025-08-21 17:41:29 -0600

    gh-pages integration with route vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 // import './App.css'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, createHashRouter, RouterProvider, } from "react-router-dom";
 
 import {
   LandingPage,
   LandingPageAlt
 } from './pages/index.js';
 
-const router = createBrowserRouter(
+const router = createHashRouter( //createBrowserRouter(No gh-pages)
   createRoutesFromElements([
     <Route path="/" element={<LandingPage />} />,
     <Route path="/alt" element={<LandingPageAlt />} />,
